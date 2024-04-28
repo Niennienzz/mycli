cb8a656f3b5e291e2fda3b42e64c63a3b9371583
Joe Zhou <zhehui.zhou@outlook.com>
2024-04-28 02:47:50 -0400

    share sha256

--- src/process/chacha.rs
+++ src/process/chacha.rs
@@ -3,16 +3,18 @@ use std::io::Read;
 use chacha20poly1305::{aead::{Aead, AeadCore, KeyInit, OsRng}, ChaCha20Poly1305};
 use chacha20poly1305::aead::generic_array::GenericArray;
 use chacha20poly1305::aead::generic_array::typenum::Unsigned;
-use sha2::{Digest, Sha256};
 
 use super::{
     b64_url_safe_no_pad_decode,
     b64_url_safe_no_pad_encode,
+    str_to_sha256_vec,
 };
 
 pub fn process_decrypt(user_key: &str, reader: &mut dyn Read) -> anyhow::Result<String> {
     // Use the user key to construct the cipher.
-    let cipher = user_key_to_cipher(user_key);
+    let cipher = ChaCha20Poly1305::new(
+        GenericArray::from_slice(&str_to_sha256_vec(user_key)),
+    );
 
     // Read the input.
     let mut buf = String::new();
@@ -33,7 +35,9 @@ pub fn process_decrypt(user_key: &str, reader: &mut dyn Read) -> anyhow::Result<
 
 pub fn process_encrypt(user_key: &str, reader: &mut dyn Read) -> anyhow::Result<String> {
     // Use the user key to construct the cipher.
-    let cipher = user_key_to_cipher(user_key);
+    let cipher = ChaCha20Poly1305::new(
+        GenericArray::from_slice(&str_to_sha256_vec(user_key)),
+    );
 
     // Generate a random nonce.
     let nonce = ChaCha20Poly1305::generate_nonce(&mut OsRng);
@@ -53,17 +57,6 @@ pub fn process_encrypt(user_key: &str, reader: &mut dyn Read) -> anyhow::Result<
     }
 }
 
-fn user_key_to_cipher(user_key: &str) -> ChaCha20Poly1305 {
-    let bytes = user_key.trim().as_bytes().to_vec();
-    let mut hasher = Sha256::new();
-    hasher.update(bytes);
-    let result = hasher.finalize();
-    let mut hash_bytes = [0u8; 32];
-    hash_bytes.copy_from_slice(&result[..]);
-    hash_bytes.to_vec();
-    ChaCha20Poly1305::new(GenericArray::from_slice(&hash_bytes))
-}
-
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -71,12 +64,11 @@ mod tests {
     #[test]
     fn test_process_encrypt_decrypt() {
         let user_key = "test-key";
-        let cipher = user_key_to_cipher(user_key);
         let input = "test-input";
         let mut reader = input.as_bytes();
         let encrypted = process_encrypt(user_key, &mut reader).unwrap();
         let mut reader = encrypted.as_bytes();
         let decrypted = process_decrypt(user_key, &mut reader).unwrap();
-        assert_eq!(decrypted, input.trim());
+        assert_eq!(input, decrypted);
     }
 }
--- src/process/mod.rs
+++ src/process/mod.rs
@@ -1,4 +1,5 @@
 use base64::{Engine as _, engine::general_purpose};
+use sha2::{Digest, Sha256};
 
 pub use chacha::{process_decrypt, process_encrypt};
 
@@ -14,4 +15,16 @@ fn b64_url_safe_no_pad_encode(data: &[u8]) -> String {
 // This can be shared between ChaCha and JWT.
 fn b64_url_safe_no_pad_decode(data: &str) -> Result<Vec<u8>, base64::DecodeError> {
     general_purpose::URL_SAFE_NO_PAD.decode(data)
+}
+
+// Helper function to hash a string to a 32-byte vector.
+// This can be shared between ChaCha and JWT.
+fn str_to_sha256_vec(user_key: &str) -> Vec<u8> {
+    let bytes = user_key.trim().as_bytes().to_vec();
+    let mut hasher = Sha256::new();
+    hasher.update(bytes);
+    let result = hasher.finalize();
+    let mut hash_bytes = [0u8; 32];
+    hash_bytes.copy_from_slice(&result[..]);
+    hash_bytes.to_vec()
 }
